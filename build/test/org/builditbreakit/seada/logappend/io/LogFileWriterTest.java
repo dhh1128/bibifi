2043bf3f89784f34cc0246d036bb15fdb84378c7
Alan Wilkinson <awilkin2@gmail.com>
2015-05-10 13:52:10 -0400

    Add crypto and I/O code. It is not the most efficient but it should get us going for now.

---
+++ build/test/org/builditbreakit/seada/logappend/io/LogFileWriterTest.java
@@ -0,0 +1,53 @@
+package org.builditbreakit.seada.logappend.io;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.builditbreakit.seada.common.data.EquivalenceUtil;
+import org.builditbreakit.seada.common.data.GalleryState;
+import org.builditbreakit.seada.common.data.VisitorType;
+import org.builditbreakit.seada.common.io.LogFileReader;
+import org.junit.Test;
+
+public class LogFileWriterTest {
+	@Test
+	public void testEmptyGalleryStateSerialization() throws IOException,
+			ClassNotFoundException {
+		testSerialization(new GalleryState());
+	}
+
+	@Test
+	public void testGalleryStateSerialization() throws IOException,
+			ClassNotFoundException {
+		GalleryState state = new GalleryState();
+		long time = 1;
+		state.arriveAtBuilding(time++, "Bob", VisitorType.EMPLOYEE);
+		state.arriveAtRoom(time++, "Bob", VisitorType.EMPLOYEE, 101);
+		state.arriveAtBuilding(time++, "Jill", VisitorType.EMPLOYEE);
+		state.arriveAtBuilding(time++, "Alice", VisitorType.EMPLOYEE);
+		state.arriveAtRoom(time++, "Jill", VisitorType.EMPLOYEE, 101);
+		state.depart(time++, "Bob", VisitorType.EMPLOYEE);
+		state.arriveAtBuilding(time++, "John", VisitorType.GUEST);
+		state.depart(time++, "John", VisitorType.GUEST);
+		state.arriveAtBuilding(time++, "John", VisitorType.GUEST);
+		state.depart(time++, "John", VisitorType.GUEST);
+
+		testSerialization(state);
+	}
+
+	private void testSerialization(GalleryState galleryState) throws IOException {
+		String password = "secret";
+		
+		File logFile = File.createTempFile("bibifi-test", "tmp");
+		logFile.deleteOnExit();
+		
+		LogFileWriter writer = new LogFileWriter(logFile);
+		writer.write(galleryState, password);
+		
+		LogFileReader reader = new LogFileReader(logFile);
+		GalleryState recoveredGalleryState = reader.read(password);
+		
+		EquivalenceUtil.assertEquivalent(galleryState, recoveredGalleryState);
+	}
+
+}
