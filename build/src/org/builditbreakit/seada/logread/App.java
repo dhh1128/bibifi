1ebad14a9350f3f9992fdab18a20bb358f94e371
Daniel Hardman <daniel.hardman@gmail.com>
2015-05-11 22:18:39 -0600

    first cut at logread cmdline

--- build/src/org/builditbreakit/seada/logread/App.java
+++ build/src/org/builditbreakit/seada/logread/App.java
@@ -1,14 +1,18 @@
 package org.builditbreakit.seada.logread;
 
+import org.builditbreakit.seada.logread.ReadCommand;
+
 public class App {
 	public static void main(String[] args) {
-		// Super dumb code, just to provide a slightly interesting stub. Rip and replace.
-		for (String arg: args) {
-			if (arg.equals("-T")) {
-				return;
-			}
+		try {
+			ReadCommand cmd = new ReadCommand();
+			// TODO: Read persisted model. We do it here instead of earlier,
+			// because there's no point in doing that work if cmd is bad.
+			cmd.parse(args);
+			// TODO: Query the model with this cmd.
+		} catch (Throwable e) {
+			System.out.println("invalid");
+			System.exit(255);
 		}
-		System.out.println("invalid");
-		System.exit(255);
 	}
 }
---
+++ build/test/org/builditbreakit/seada/logread/ReadCommandTest.java
@@ -0,0 +1,43 @@
+package org.builditbreakit.seada.logread;
+
+import junit.framework.TestCase;
+import org.builditbreakit.seada.logappend.BatchProcessor;
+
+public class ReadCommandTest extends TestCase {
+
+	private static String[] validCommands = {
+		"-K secret -S logfile",
+		"-K secret -R -E fred logfile",
+		"-K secret -T -E fred logfile",
+		"-K secret -I -E fred -G barney logfile",
+	};
+
+	private static String[] invalidCommands = {
+		"-K secret -S", // no logfile
+		"-K secret -X -E fred logfile", // bad style
+		"-K secret -T -E fr0ed logfile", // bad guestname
+		"-K secret -I logfile", // no visitors
+	};
+	
+	public void testValid() {
+		for (String line: validCommands) {
+			try {
+				ReadCommand cmd = new ReadCommand();
+				cmd.parse(BatchProcessor.splitLine(line));
+			} catch (Throwable e) {
+				fail("Cmdline \"" + line + "\" should have been valid, but wasn't: " + e.toString());
+			}
+		}
+	}
+
+	public void testInvalid() {
+		for (String line: invalidCommands) {
+			try {
+				ReadCommand cmd = new ReadCommand();
+				cmd.parse(BatchProcessor.splitLine(line));
+				fail("Cmdline \"" + line + "\" should not have been valid, but was.");
+			} catch (Throwable e) {
+			}
+		}
+	}
+}
