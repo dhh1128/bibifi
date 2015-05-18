3f99920bdab18444c37d0f38a3b058523a9c191f
Alan Wilkinson <awilkin2@gmail.com>
2015-05-18 17:02:26 -0400

    Switch from Java provided crypto to Bouncy Castle

--- build/src/org/builditbreakit/seada/common/io/Crypto.java
+++ build/src/org/builditbreakit/seada/common/io/Crypto.java
@@ -1,90 +1,115 @@
 package org.builditbreakit.seada.common.io;
 
-import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
-import javax.crypto.Cipher;
-import javax.crypto.Mac;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
+import org.bouncycastle.crypto.BlockCipher;
+import org.bouncycastle.crypto.BufferedBlockCipher;
+import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.Mac;
+import org.bouncycastle.crypto.digests.SHA1Digest;
+import org.bouncycastle.crypto.engines.BlowfishEngine;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.modes.CBCBlockCipher;
+import org.bouncycastle.crypto.paddings.BlockCipherPadding;
+import org.bouncycastle.crypto.paddings.PKCS7Padding;
+import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.crypto.params.ParametersWithIV;
+import org.bouncycastle.util.Arrays;
 
 public final class Crypto {
 	private static final int KEY_SIZE = 16;
-	public static final String CIPHER_KEY_TYPE = "Blowfish";
-	public static final String KEY_HASH_NAME = "SHA-1";
 	
-	public static final String CIPHER_NAME = "Blowfish/CBC/PKCS5Padding";
 	public static final String NATIVE_PRNG_NAME = "NativePRNGNonBlocking";
 	public static final String BACKUP_PRNG_NAME = "SHA1PRNG";
 	public static final String MAC_NAME = "HmacSHA1";
-	public static final String MAC_KEY_TYPE = "HmacSHA1";
-	public static final int MAC_SIZE = 20;
 	public static final int IV_SIZE = 8;
 	
 	private static SecureRandom rand;
+	private static BufferedBlockCipher cipher;
+	private static Digest digest;
+	private static Mac mac;
 	
 	private Crypto() {
 		super();
 	}
 	
-	public static Key generateBaseKey(String password) {
+	public static byte[] generateKey(String password) {
 		try {
-			MessageDigest hash = MessageDigest.getInstance(KEY_HASH_NAME);
-			byte[] hashedPassword = hash.digest(password.getBytes());
-			return new SecretKeySpec(hashedPassword, 0, KEY_SIZE,
-					CIPHER_KEY_TYPE);
-		} catch (NoSuchAlgorithmException e) {
+			Digest digest = getDigest();
+			if (digest.getDigestSize() < KEY_SIZE) {
+				throw new InvalidKeyException("Cannot create key of size "
+						+ KEY_SIZE + " with " + digest.getAlgorithmName());
+			}
+			byte[] hash = new byte[digest.getDigestSize()];
+			digest.doFinal(hash, 0);
+			return Arrays.copyOf(hash, KEY_SIZE);
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 	
-	public static Mac getMac(Key key) {
-		try {
-			Mac mac = Mac.getInstance(MAC_NAME);
-			mac.init(generateMacKey(key));
-			return mac;
-		} catch (NoSuchAlgorithmException | InvalidKeyException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	public static Cipher getEncryptingCipher(Key key, byte[] iv) {
-		return getCipher(key, iv, Cipher.ENCRYPT_MODE);
-	}
-
-	public static Cipher getDecryptingCipher(Key key, byte[] iv) {
-		return getCipher(key, iv, Cipher.DECRYPT_MODE);
-	}
-
 	public static byte[] generateIV() {
 		byte[] result = new byte[IV_SIZE];
 		getRandom().nextBytes(result);
 		return result;
 	}
-	
-	private static Cipher getCipher(Key key, byte[] iv, int mode) {
-		try {
-			Cipher cipher = Cipher.getInstance(CIPHER_NAME);
-			IvParameterSpec ivParam = new IvParameterSpec(iv);
-			cipher.init(mode, generateCipherKey(key), ivParam);
-			return cipher;
-		} catch (NoSuchPaddingException | NoSuchAlgorithmException
-				| InvalidKeyException | InvalidAlgorithmParameterException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	private static Key generateCipherKey(Key baseKey) {
-		return new SecretKeySpec(baseKey.getEncoded(), CIPHER_KEY_TYPE);
+
+	public static Mac getMac(byte[] key) {
+		Mac result = getMac();
+		result.init(getMacParams(key));
+		return result;
 	}
 
-	private static Key generateMacKey(Key baseKey) {
-		return new SecretKeySpec(baseKey.getEncoded(), MAC_KEY_TYPE);
+	public static BufferedBlockCipher getEncryptingCipher(byte[] key, byte[] iv) {
+		return initCipher(getCipher(), true, key, iv);
+	}
+
+	public static BufferedBlockCipher getDecryptingCipher(byte[] key, byte[] iv) {
+		return initCipher(getCipher(), false, key, iv);
+	}
+
+	private static BufferedBlockCipher initCipher(BufferedBlockCipher cipher,
+			boolean forEncryption, byte[] key, byte[] iv) {
+		cipher.init(forEncryption, getCipherParams(key, iv));
+		return cipher;
+	}
+	
+	private static Digest getDigest() {
+		if(digest == null) {
+			digest = new SHA1Digest();
+		}
+		return digest;
+	}
+
+	private static Mac getMac() {
+		if(mac == null) {
+			Digest digest = new SHA1Digest();
+			mac = new HMac(digest);
+		}
+		return mac;
+	}
+	
+	private static CipherParameters getCipherParams(byte[] key, byte[] iv) {
+		KeyParameter keyParam = new KeyParameter(key);
+		return new ParametersWithIV(keyParam, iv);
+	}
+	
+	private static CipherParameters getMacParams(byte[] key) {
+		return new KeyParameter(key);
+	}
+	
+	private static BufferedBlockCipher getCipher() {
+		if (cipher == null) {
+			BlockCipher engine = new BlowfishEngine();
+			CBCBlockCipher mode = new CBCBlockCipher(engine);
+			BlockCipherPadding padding = new PKCS7Padding();
+			cipher = new PaddedBufferedBlockCipher(mode, padding);
+		}
+		return cipher;
 	}
 
 	/** Not Thread Safe. Single-threaded apps only */
