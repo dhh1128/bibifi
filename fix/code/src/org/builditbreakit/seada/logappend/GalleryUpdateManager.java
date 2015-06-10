325d0dae81f75e92aa8ac942db95ed4611c9d0ac
Daniel Hardman <daniel.hardman@gmail.com>
2015-06-10 00:26:48 -0400

    fix issue where we truncate trailing slash in log filename

--- fix/code/src/org/builditbreakit/seada/logappend/GalleryUpdateManager.java
+++ fix/code/src/org/builditbreakit/seada/logappend/GalleryUpdateManager.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.builditbreakit.seada.common.data.GalleryState;
 import org.builditbreakit.seada.common.io.LogFileReader;
@@ -13,13 +15,20 @@ public class GalleryUpdateManager {
 	
 	private Map<String, GalleryUpdate> states = new HashMap<String, GalleryUpdate>();
 	
+	private static final Pattern cantBeFile = Pattern.compile(".*/$");
+	
 	/**
 	 * Find the existing gallery state for a particular path -- or create a new one, if
 	 * the path doesn't exist yet. This allows the same state to be reused without being
 	 * read/written between each use.
 	 */
 	public GalleryUpdate getGalleryFor(AppendCommand cmd) throws IOException, SecurityException {
-		File f = new File(cmd.getLogfile());
+		String lf = cmd.getLogfile();
+		Matcher m = cantBeFile.matcher(lf);
+		if (m.matches()) {
+			throw new IOException("Bad logfile.");
+		}
+		File f = new File(lf);
 		String password = cmd.getToken();
 		// Convert to canonical version of path so different paths to the same logfile
 		// are resolved to the same thing.
